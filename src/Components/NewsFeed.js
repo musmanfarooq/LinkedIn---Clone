b3e4d85d4b9c614e21b350fd434e309fe67445f4
Usman Farooq <m.usmanfarooq753@gmail.com>
2022-12-07 15:41:55 +0500

    added firebase

--- src/Components/NewsFeed.js
+++ src/Components/NewsFeed.js
@@ -5,13 +5,25 @@ import {
   Image,
   Subscriptions,
 } from "@mui/icons-material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase";
 import InputOptions from "./InputOptions";
 import "./NewsFeed.css";
 import Post from "./Post";
 
 function NewsFeed() {
   const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) =>
+      setPost(
+        snapshot.docs.map((doc) => ({
+          id: docs.id,
+          data: doc.data(),
+        }))
+      )
+    );
+  }, []);
 
   function sendPostHandler(e) {
     e.preventDefault();
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,17 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyD9DJcPl48_RozCe2rNEVKwunzVcufZru0",
-    authDomain: "linkedin---clone-79f15.firebaseapp.com",
-    projectId: "linkedin---clone-79f15",
-    storageBucket: "linkedin---clone-79f15.appspot.com",
-    messagingSenderId: "171257879741",
-    appId: "1:171257879741:web:89371d2e9e9e2836b6ebd4"
-  };
+  apiKey: "AIzaSyD9DJcPl48_RozCe2rNEVKwunzVcufZru0",
+  authDomain: "linkedin---clone-79f15.firebaseapp.com",
+  projectId: "linkedin---clone-79f15",
+  storageBucket: "linkedin---clone-79f15.appspot.com",
+  messagingSenderId: "171257879741",
+  appId: "1:171257879741:web:89371d2e9e9e2836b6ebd4",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+export { auth, db };
