d8bc30ac5e529ca0a2d87f7dbac4dda5789a6d41
Usman Farooq <m.usmanfarooq753@gmail.com>
2022-12-08 15:25:33 +0500

    auth completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { Navbar } from "./Components/Navbar";
 import RightSideBar from "./Components/RightSideBar";
 import NewsFeed from "./Components/NewsFeed";
 import LoginPage from "./Pages/LoginPage";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectUser } from "./features/userSlice";
+import { auth } from "./firebase";
+import { login, logout } from "./features/userSlice";
 
 function App() {
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        console.log("islogout");
+        dispatch(logout());
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [dispatch]);
 
   return (
     <div className="app">
