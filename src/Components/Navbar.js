012a394fa214ae359e64a0de05b8d9201199cffd
Usman Farooq <m.usmanfarooq753@gmail.com>
2022-12-09 14:31:35 +0500

    added sign out

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import SearchIcon from "@mui/icons-material/Search";
 import NavBarIcons from "./NavBarIcons";
-import { Home, Group, Work, Message, Notifications } from "@mui/icons-material";
+import {
+  Home,
+  Group,
+  Work,
+  Message,
+  Notifications,
+  ArrowDropDown,
+} from "@mui/icons-material";
+import { auth } from "../firebase";
 
 export const Navbar = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const signoutHandler = () => {
+    setIsLoading((current) => !current);
+  };
+
+  const logoutHandler = () => {
+    auth.signOut();
+  };
+
   return (
     <div className="navbar">
       <div className="navbar__left">
@@ -27,6 +45,12 @@ export const Navbar = () => {
           avatar="https://occ-0-1242-64.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABREqJW8jJAoSQksyGaNcnbGKith54H6eyxJn--GI-vumuX4-378QiUW_AarRAATIHPEtYpAJCcBU6vwAIRFUE0HevMe-lHD4mIH6.png?r=e31"
           title="me"
         />
+        <ArrowDropDown onClick={signoutHandler} className="navbar__dropdown" />
+        {isLoading && (
+          <div className="navbar__signout">
+            <p onClick={logoutHandler}>Logout</p>
+          </div>
+        )}
       </div>
     </div>
   );
